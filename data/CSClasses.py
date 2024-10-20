96b05b8d7ac8194ea08fcec9c0cbf1cfe5689207
Yuki Suwabe <ys462@cornell.edu>
2024-10-19 23:13:32 -0400

    Demo Code

---
+++ data/CSClasses.py
@@ -0,0 +1,22 @@
+import json
+
+import requests
+
+url = "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA24&subject="
+
+with open("CSClasses.json", "w") as f:
+    try:
+        f.write('{\n"classes": { \n')
+        url_request = url + "CS"
+        response = requests.get(url_request)
+        classes = response.json()
+        print(url_request)
+        if response.status_code == 200:
+            f.write('"CS": ')
+            json.dump(classes["data"]["classes"], f, ensure_ascii=False, indent=4)
+            # f.write(", \n")
+        else:
+            print("Error: ", response.status_code)
+        f.write("}\n}")
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
---
+++ data/demoCleanup.py
@@ -0,0 +1,21 @@
+import json
+
+with open("CSClasses.json", "r") as classes:
+    file = json.load(classes)
+    f = open("DemoData.txt", "w")
+    for subjects in file["classes"]:
+        # print(file["classes"][subjects])
+        for c in file["classes"][subjects]:
+            # print(c["subject"] + " " + c["catalogNbr"])
+            f.write("class: " + c["subject"] + " " + c["catalogNbr"] + "\n")
+            f.write("class title: " + c["titleLong"] + "\n")
+            if c["description"]:
+                f.write("description: " + c["description"] + "\n")
+            if c["catalogOutcomes"]:
+                f.write("outcome: ")
+                for outcome in c["catalogOutcomes"]:
+                    f.write(outcome + ", ")
+                f.write("\n")
+            if c["catalogDistr"]:
+                f.write("distribution categories: " + c["catalogDistr"] + "\n")
+            f.write("\n")
